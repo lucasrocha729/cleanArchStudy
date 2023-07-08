7269102030733352b39de83123acf54bca166e36
lucasrocha729 <lucasrocha.0822@gmail.com>
2023-07-08 17:08:57 -0300

    test: add validation in signUpContoller if missing param "passwordConfirmation"

--- src/presentation/controllers/signut.ts
+++ src/presentation/controllers/signut.ts
@@ -4,7 +4,7 @@ import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: httpRequest): httpResponse {
-    const requiredFields = ['name', 'email', 'password']
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
     for (const element of requiredFields) {
       if (!httpRequest.body[element]) return badRequest(new MissingParamError(element))
