24d2d199b6d68870ae17187294203a8f6ce205c4
lucasrocha729 <lucasrocha.0822@gmail.com>
2023-07-26 15:23:20 -0300

    feat: ensure add account

---
+++ src/infra/criptography/db/mongodb/account-repository/account.ts
@@ -0,0 +1,13 @@
+import { AccountModel } from './../../../../../domain/models/account'
+import { AddAccountReporitory } from '../../../../../data/protocols/add-account-reporitory'
+import { AddAccountModel } from '../../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helpers'
+
+export class AccountMongoRepository implements AddAccountReporitory {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+
+    return result as unknown as AccountModel
+  }
+}
